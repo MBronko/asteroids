6b4ed500c0d59c533c571f9a1d86b049b3c2abfe
MBronko <michal.brono@protonmail.com>
2021-05-24 12:45:36 +0200

    Fix rotation towards center in utils

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,5 @@
 int main() {
     srand(time(nullptr));
 
-    position test = {WIDTH_CENTER-4, HEIGHT_CENTER - 3};
-
-//    for (int i = 0; i < 50; i++){
-        position test2 = velocity_towards_center(test);
-
-        std::cout << test2.x << " " << test2.y << std::endl;
-//    }
-    std::cout << std::atan(4.0/3) << " " << std::atan(-4.0/3) << " " << std::atan(4.0/-3) << " " << std::atan(-4.0/-3) << " " << std::endl;
-
-    std::cout << std::endl << to_degree(std::atan(4.0/3));
-
     return 0;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -23,7 +23,7 @@ position rotate_pos(position pos, double degree){
     double rad = to_radians(degree);
 
     double x = pos.x * std::cos(rad) - pos.y * std::sin(rad);
-    double y = pos.y * std::cos(rad) + pos.x * std::sin(rad);
+    double y = - pos.y * std::cos(rad) - pos.x * std::sin(rad);
 
     return {x, y};
 }
@@ -34,10 +34,12 @@ position velocity_towards_center(position pos){
 
     double new_rotation = 2 * M_PI - std::atan(y_off/x_off);
 
-    position vel = {MAX_ASTEROID_VELOCITY, 0};
+//    position vel = {MAX_ASTEROID_VELOCITY, 0};
+    position vel = {random_range(MIN_ASTEROID_VELOCITY, MAX_ASTEROID_VELOCITY), 0};
+
     double rotation_offset = random_range(-NEW_ASTEROID_DEGREE_RANGE, NEW_ASTEROID_DEGREE_RANGE);
-    std::cout << x_off << " " << y_off << " " << std::atan(y_off/x_off) << " " << to_degree(new_rotation) << " " << rotation_offset << std::endl;
-    rotation_offset = 0;
+    rotation_offset += x_off < 0 ? 180 : 0;  // flip by 180 degrees if point is on the right side of board
+
     return rotate_pos(vel, shorten_rotation(to_degree(new_rotation) + rotation_offset));
 }
 
